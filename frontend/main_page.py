c293a83a8bf071f3a4feb537d9b56a43920e5412
lorantszabo98 <64622519+lorantszabo98@users.noreply.github.com>
2024-04-12 01:19:30 +0200

    Geolocation error handling

--- frontend/main_page.py
+++ frontend/main_page.py
@@ -4,43 +4,45 @@ from utils.get_location import get_my_location
 import pandas as pd
 import pickle
 
+
 st.title('Can I play badminton right now?')
 
 lat, lon = get_my_location()
-# city = 'Budapest'
-# lat = '47.4979'
-# lon = '19.0402'
 
-description, temperature, humidity, wind_speed = get_weather_data(lat, lon)
+if lat is not None:
 
-if description is not None:
+    description, temperature, humidity, wind_speed = get_weather_data(lat, lon)
 
-    metrics_toogle = st.toggle('Switch to metric')
-    st.caption('Weather data based :blue[on your location]:')
+    if description is not None:
 
-    if metrics_toogle:
-        # temperature, wind_speed = convert_metrics(temperature, wind_speed)
-        col1, col2, col3, col4 = st.columns(4)
-        col1.metric("Description", f'{str(description)} ')
-        col2.metric("Temperature", f'{str(round(temperature-273.15, 2))} °C')
-        col3.metric("Humidity", f'{str(humidity)} %')
-        col4.metric("Wind Speed", f'{str(round(wind_speed*3.6, 1))} km/h')
+        metrics_toogle = st.toggle('Switch to metric')
+        st.caption('Weather data based :blue[on your location]:')
+
+        if metrics_toogle:
+            # temperature, wind_speed = convert_metrics(temperature, wind_speed)
+            col1, col2, col3, col4 = st.columns(4)
+            col1.metric("Description", f'{str(description)} ')
+            col2.metric("Temperature", f'{str(round(temperature-273.15, 2))} °C')
+            col3.metric("Humidity", f'{str(humidity)} %')
+            col4.metric("Wind Speed", f'{str(round(wind_speed*3.6, 1))} km/h')
+        else:
+            col1, col2, col3, col4 = st.columns(4)
+            col1.metric("Description", f'{str(description)} ')
+            col2.metric("Temperature", f'{str(temperature)} K')
+            col3.metric("Humidity", f'{str(humidity)} %')
+            col4.metric("Wind Speed", f'{str(wind_speed)} m/s')
+
+        classification_result = classify_weather_data(description, temperature, humidity, wind_speed)
+        current_weather = pd.DataFrame(classification_result)
+
+        dtree_model = pickle.load(open('./backend/trained_classifier_model/dt_model.sav', 'rb'))
+        play_badminton_prediction = dtree_model.predict(current_weather)
+
+        if play_badminton_prediction == 1:
+            st.success('Yes, you can play badminton!')
+        else:
+            st.error('No, unfortunately the weather conditions are not suitable for badminton!')
     else:
-        col1, col2, col3, col4 = st.columns(4)
-        col1.metric("Description", f'{str(description)} ')
-        col2.metric("Temperature", f'{str(temperature)} K')
-        col3.metric("Humidity", f'{str(humidity)} %')
-        col4.metric("Wind Speed", f'{str(wind_speed)} m/s')
-
-    classification_result = classify_weather_data(description, temperature, humidity, wind_speed)
-    current_weather = pd.DataFrame(classification_result)
-
-    dtree_model = pickle.load(open('./backend/trained_classifier_model/dt_model.sav', 'rb'))
-    play_badminton_prediction = dtree_model.predict(current_weather)
-
-    if play_badminton_prediction == 1:
-        st.success('Yes, you can play badminton!')
-    else:
-        st.error('No, unfortunately the weather conditions are not suitable for badminton!')
+        st.error('Failed to retrieve weather data. Please try again later.')
 else:
-    st.error('Failed to retrieve weather data. Please try again later.')
+    st.error('Failed to retrieve GPS data, please enable location permission in your browser!')
--- frontend/utils/get_location.py
+++ frontend/utils/get_location.py
@@ -3,5 +3,11 @@ from streamlit_js_eval import get_geolocation
 
 def get_my_location():
 
-    location = get_geolocation()
-    return location['coords']['latitude'], location['coords']['longitude']
+    location = get_gelocation()
+
+    if location is not None:
+
+        return location['coords']['latitude'], location['coords']['longitude']
+
+    else:
+        return None, None
